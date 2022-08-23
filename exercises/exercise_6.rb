0148b9e6f8aa2882a726d7942d384170bf81d846
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-08-23 21:25:17 +0000

    change stores for some employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,11 +11,14 @@ puts "----------"
 # Your code goes here ...
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 
-@store1.employees.create(first_name: "Bobert", last_name: "Bobbington", hourly_rate: 640)
+@store1.employees.create(first_name: "Bobert", last_name: "Bobbington", hourly_rate: 90)
 
 @store1.employees.create(first_name: "Tom", last_name: "Thomas", hourly_rate: 40)
 
-@store1.employees.create(first_name: "Reed", last_name: "Richards", hourly_rate: 50)
+@store2.employees.create(first_name: "Reed", last_name: "Richards", hourly_rate: 55)
 
-@store1.employees.create(first_name: "Ben", last_name: "Grimm", hourly_rate: 50)
+@store2.employees.create(first_name: "Ben", last_name: "Grimm", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Sue", last_name: "Storm", hourly_rate: 80)
+
 
